1c550320e38807c65109578163e61df0660bf486
Thiago Aires <Thiago Aires>
2016-06-09 00:28:15 -0300

    aula 3 - js: Programando na linguagem da web

--- alura-js/projeto/calcula-imc.js
+++ alura-js/projeto/calcula-imc.js
@@ -1,14 +1,23 @@
 // peso / altura ²
-var peso = 100;
-var altura = 0;
+
+var tdPeso = document.getElementById('peso-2');
+var tdAltura = document.getElementById('altura-2');
+// var peso = 100;
+// var altura = 0;
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var paciente = {"peso": tdPeso.textContent, "altura": tdAltura.textContent};
 
 var ehDiferenteDeZero = (altura != 0); 
 
 if (ehDiferenteDeZero){
-
-	var imc = peso / (altura * altura);
+	var tdImc = document.getElementById('imc-2');
+	var imc = paciente.peso / (paciente.altura * paciente.altura);
 
 	console.log(imc);
+	tdImc.textContent = imc;
 
 } else {
 	console.log('é zero')
