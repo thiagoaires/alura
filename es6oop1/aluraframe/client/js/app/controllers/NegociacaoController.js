75b2250df5e14b8aca5e7ba555f63d856d807850
Thiago Aires <thiago@MAC-016.local>
2018-02-23 10:11:56 -0300

    es6 avançado 1 -> aula 3, problema com datas

---
+++ es6oop1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,24 @@
+class NegociacaoController{
+
+    constructor(){
+
+        let $ = document.querySelector.bind(document);
+        
+        this._inputData       = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor      = $('#valor');
+
+    }
+
+    adiciona(event){
+
+        event.preventDefault();
+
+        console.log(typeof(this._inputData.value));
+
+        console.log(this._inputData.value.split());
+
+        // #TODO Adicionar a negociaçao em uma lista
+    }
+
+}
