9b575aecfef0a72f6b78e63bcfa9ada41c113a62
Thiago Aires <thiago@MAC-016.local>
2018-02-26 09:59:06 -0300

    ES6 - Avançado 1: Aula 3

--- es6oop1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ es6oop1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -15,13 +15,32 @@ class NegociacaoController{
         event.preventDefault();
 
         let data = new Date(
-            //this._inputData.value.split('-')
-            
+            ...this._inputData.value
+            .split('-')
+            /*
+            .map(function(item, indice){
+                return item - indice % 2;
+            })
+            */
+            .map((item, indice) => item - indice % 2)
+
+            /* ####### 
+            qdo se há uma unica instruçao no arrow function(iten - indice...)
+            pode-se omitir o bloco ( colchete, {} ) e colocar inline
+
+            outra coisa, como soh tem uma instruçao, pode-se remover o return e o ;
+            ####### */
         );
 
         console.log(data);
 
-        // #TODO Adicionar a negociaçao em uma lista
+        let negociacao = new Negociacao(
+            data, 
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
     }
 
 }
