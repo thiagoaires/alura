249069e933a1fb894d8883664eaed06712367925
Thiago Aires <thiago@MAC-016.local>
2018-03-02 11:52:39 -0300

    Node.js -> instalando e rodando primeira app no express

--- nodejs-inovando/casadocodigo/app.js
+++ nodejs-inovando/casadocodigo/app.js
@@ -2,6 +2,14 @@ let express = require('express');
 let app = express();
 let porta = 3000;
 
+let cospeHtml = function(conteudo){
+    return `<html><body>${conteudo}</body></html>`;
+}
+
+app.get('/produtos', function(req, res){
+    res.send(cospeHtml('<h1>pagina de produtos</h1>'));
+})
+
 app.listen(porta, function(){
     console.log(`Servidor rodando na porta ${porta}`);
 });
