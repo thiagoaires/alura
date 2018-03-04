c0114f87cbdf69352cfc6e8634442463eb3f3d8e
Thiago Aires [note] <makarrao@gmail.com>
2018-03-03 21:56:55 -0300

    Node.js -> Configruação do Express e do EJS

--- nodejs-inovando/casadocodigo/app/routes/produtos.js
+++ nodejs-inovando/casadocodigo/app/routes/produtos.js
@@ -1,8 +1,23 @@
-
 module.exports = function(app){
 
     app.get('/produtos', function(req, res){
         
-        res.render('produtos/lista');
+        let mysql = require('mysql');
+        let connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'casadocodigo_nodejs'
+        });
+
+
+        connection.query('select * from livros',function(err, results){
+            //res.send(results);
+            res.render('produtos/lista', {lista: results})
+            console.log('resultado da query');
+        });
+
+        connection.end();
+    
     });
 };
