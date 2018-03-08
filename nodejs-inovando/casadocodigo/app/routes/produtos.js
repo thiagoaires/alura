123e24a0b77e9f3c7c0a3de92ed3b3cb854ab850
Thiago Aires <thiago.aires@loopix.com.br>
2018-03-08 16:38:10 -0300

    nodeJS -> Aula 2 [encerrado]

--- nodejs-inovando/casadocodigo/app/routes/produtos.js
+++ nodejs-inovando/casadocodigo/app/routes/produtos.js
@@ -1,24 +1,39 @@
-
 module.exports = function(app){
 
     app.get('/produtos', function(req, res){
 
         let connection      = app.infra.connectionFactory();
-        let produtosBanco   = app.infra.produtosBanco;
+        let produtosBanco   = new app.infra.ProdutosDAO(connection);
         
         
-        produtosBanco.lista(connection, function(erros, resultados){
+        produtosBanco.lista(function(erros, resultados){
             res.render('produtos/lista', {lista: resultados});
-            //res.send(resultados);
         });
-        
-
-        // connection.query('select * from livros',function(err, resultados){
-        //     res.render('produtos/lista', {lista: resultados});
-        // });
-
         connection.end();
+
+        
     });
 
-    app.get('produtos/detalhe', function(){});
+
+
+
+
+
+
+
+    app.get('produtos/detalhe', function(){
+
+        produtosBanco.carrega(id, callback)
+    });
+
+    app.get('produtos/detalhe', function(){
+        let connection = app.infra.connectionFactory();
+        let produtosBanco = app.infra.produtosBanco;
+
+        let produto = produtosBanco.carrega(id, callback);
+
+        if(produto){
+            produtosBanco.remove(produto, callback);
+        };
+    });
 };
