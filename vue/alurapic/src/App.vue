4de53b159fae9e00c5e6b12a63ac9ece8a406bf8
Thiago Aires <thiago.aires@loopix.com.br>
2018-03-26 11:56:44 -0300

    Vue.js - Aula 3

--- vue/alurapic/src/App.vue
+++ vue/alurapic/src/App.vue
@@ -3,7 +3,7 @@
     <h1>{{ titulo }}</h1>
     <img :src="fotos.url" :alt="fotos.titulo">
     <ul>
-      <li v-for="foto of fotos">
+      <li v-for="foto in fotos">
         <img :src="foto.url" :alt="foto.titulo">
       </li>
     </ul>
@@ -16,16 +16,14 @@ export default {
   data(){
     return{
       titulo: 'Alura Pic',
-      fotos: [
-          {
-            url: 'https://i.ytimg.com/vi/81zLH4ygz3M/hqdefault.jpg',
-            titulo: 'cachorro'
-          },{
-            url: 'https://i.ytimg.com/vi/cKB8K59layQ/hqdefault.jpg',
-            titulo: 'cachorro'
-          }
-        ]
+      fotos: []
     }
+  },
+
+  created(){
+    this.$http.get('http://localhost:3123/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
 }
 </script>
