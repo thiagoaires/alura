18ca8a6b4e37d4c85fb76948667406c1dd02272f
Thiago Aires <thiago.aires@loopix.com.br>
2018-03-29 12:18:37 -0300

    VueJS01 - Sec04-02

--- vue/alurapic/src/App.vue
+++ vue/alurapic/src/App.vue
@@ -1,10 +1,19 @@
+
+
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
     <img :src="fotos.url" :alt="fotos.titulo">
-    <ul>
-      <li v-for="foto in fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos">
+
+        <div class="painel">
+          <h2 class="painel-titulo">{{foto.titulo}}</h2>
+          <div class="painel-conteudo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          </div>
+        </div>
+        
       </li>
     </ul>
   </div>
@@ -30,4 +39,48 @@ export default {
 
 <style>
 
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva{
+    width: 100%;
+  }
+
+  /* estilo do painel */ 
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 1px;
+    background: #fc0;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 </style>
