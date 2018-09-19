a558229d4c2eb91db702c459cc21506bf37bd910
Thiago Aires <thiago.aires@MAC-016.local>
2018-09-19 17:05:55 -0300

    React-router

--- React02/instalura/src/index.js
+++ React02/instalura/src/index.js
@@ -11,11 +11,19 @@ import './css/timeline.css';
 import {Route, BrowserRouter} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
+function verificaAutenticacao(next, replace){
+  console.log('verifica')
+  if(localStorage.getItem('auth-token') !== null){
+    return true
+  }
+}
+
+
 ReactDOM.render(
   <BrowserRouter>
     <Fragment>
       <Route exact path="/" component={Login} />
-      <Route path="/timeline" component={App} />
+      <Route path="/timeline" render={() => verificaAutenticacao() ? <App/> : <Login />}/>
     </Fragment>
   </BrowserRouter>, document.getElementById('root')
 );
